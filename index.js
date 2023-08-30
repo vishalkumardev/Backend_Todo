f2a3f14ec8f0ce60fd7f9ab70d1d7d80c030f83a
vishalkumar <5060vishalkumar@gmail.com>
2023-08-30 17:03:07 +0530

    update in index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const cors = require("cors");
 require("./conn/Conn");
 app.use(express.json());
+app.use(cors());
 app.use(require("./Router/Notes"));
 app.use(require("./Router/User"));
 
